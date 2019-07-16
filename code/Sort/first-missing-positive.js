d9ad3c50de32cd0a916323122917ac22bd02b944
Scarlett <scarlettkkszekai@gmail.com>
2019-07-16 09:50:56 +0800

    finish first-missing-positive

--- code/Sort/first-missing-positive.js
+++ code/Sort/first-missing-positive.js
@@ -4,5 +4,18 @@
  * 题目：https://leetcode-cn.com/problems/first-missing-positive/
  */
 var firstMissingPositive = function(nums) {
-    
+    nums = nums.filter((item) => {
+    	return item > 0;
+    })
+    nums = unique(nums);
+    nums = nums.sort((a,b) => a-b);
+    for(var i = 0; i < nums.length; i++){
+    	if(nums[i] !== i + 1)
+    		return i + 1
+    }
+    return i + 1
 };
+
+function unique (arr) {
+  return Array.from(new Set(arr))
+}
