93c85a35ea998a047276ac23a938781a94ddc1a9
Scarlett <scarlettkkszekai@gmail.com>
2019-07-17 09:42:22 +0800

    finish maximum-gap

--- code/Sort/maximum-gap.js
+++ code/Sort/maximum-gap.js
@@ -4,5 +4,14 @@
  * 题目：https://leetcode-cn.com/problems/maximum-gap/
  */
 var maximumGap = function(nums) {
-    
+    nums.sort((a, b) => {
+    	return a - b
+    })
+    var max = 0
+    for(let i = 0; i < nums.length - 1; i++){
+    	let gap = nums[i+1] - nums[i]
+    	if(gap > max)
+    		max = gap;
+    }
+    return max;
 };
