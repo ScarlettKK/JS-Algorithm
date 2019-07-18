c7bbdf64f7809c430a213a768beb7987cb6f57e8
Scarlett <scarlettkkszekai@gmail.com>
2019-07-18 12:52:06 +0800

    finish sort-array-by-parity-ii

--- code/Sort/sort-array-by-parity-ii.js
+++ code/Sort/sort-array-by-parity-ii.js
@@ -4,5 +4,22 @@
  * 题目：https://leetcode-cn.com/problems/sort-array-by-parity-ii/
  */
 var sortArrayByParityII = function(A) {
-    
+    var len = A.length;
+    var res = [];
+    // 保存当前奇位和偶位
+    var even = 0, odd = 1;
+    // 遍历数组
+    for(let i = 0; i < len; i ++){
+    	// 找到偶数
+    	if(A[i] % 2 === 0){
+    		// 保存下来
+    		res[even] = A[i];
+    		// 偶位前进两格
+    		even += 2;
+    	} else { // 找到奇数
+    		res[odd] = A[i];
+    		odd += 2;
+    	}
+    }
+    return res;
 };
