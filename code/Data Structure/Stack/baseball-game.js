7b97e0d69ac441944d9305d8a9e71ae881779fa9
ScarlettKK <scarlettkkszekai@gmail.com>
2019-07-26 09:43:44 +0800

    add new exercises

---
+++ code/Data Structure/Stack/baseball-game.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} ops
+ * @return {number}
+ * 题目：https://leetcode-cn.com/problems/baseball-game/
+ */
+ var calPoints = function(ops) {
+    
+ };
---
+++ code/Data Structure/Stack/maximal-rectangle.js
@@ -0,0 +1,8 @@
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ * 题目：https://leetcode-cn.com/problems/maximal-rectangle/
+ */
+ var maximalRectangle = function(matrix) {
+    
+ };
