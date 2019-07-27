cced9161e8c65506c3a53f5183c708230030490b
ScarlettKK <scarlettkkszekai@gmail.com>
2019-07-27 09:23:44 +0800

    finish baseball-game

--- code/Data Structure/Stack/baseball-game.js
+++ code/Data Structure/Stack/baseball-game.js
@@ -4,5 +4,28 @@
  * 题目：https://leetcode-cn.com/problems/baseball-game/
  */
  var calPoints = function(ops) {
-    
+ 	var res = [];
+ 	ops.forEach((item) => {
+ 		switch(item) {
+ 			case 'C':
+ 				if(res.length > 0)
+ 					res.pop()
+ 				break;
+ 			case 'D':
+ 				var len = res.length;
+ 				if(len > 0)
+ 					res.push(res[len-1]*2)
+ 				break;
+ 			case '+':
+ 				var len = res.length;
+ 				if(len > 1)
+ 					res.push(res[len-1]+res[len-2])
+ 				break;
+ 			default:
+ 				res.push(item * 1)
+ 		}
+ 	})
+    return res.reduce((total, num) => {
+	    return total + num;
+	})
  };
