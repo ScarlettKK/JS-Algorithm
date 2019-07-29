67380609bd3e79047208c397cc20938fde3e0376
ScarlettKK <scarlettkkszekai@gmail.com>
2019-07-29 17:50:06 +0800

    a start of maximal-rectangle

--- code/Data Structure/Stack/maximal-rectangle.js
+++ code/Data Structure/Stack/maximal-rectangle.js
@@ -4,5 +4,18 @@
  * 题目：https://leetcode-cn.com/problems/maximal-rectangle/
  */
  var maximalRectangle = function(matrix) {
-    
+    let result = [];
+    // 至少两个1以上连着的部分
+    let reg = /1{2,}/g
+    // 把二维数组重新表达,将连续的相邻的1的位置提取出来(起始点+截止点)
+    result = matrix.map((item) => {
+    	let str = item.join('');
+    	let r = reg.exec(str);
+    	let res = []
+    	while(r) {
+    		rs.push([r.index, r.index + r[0].length -1])
+    		r = reg.exec(str);
+    	}
+    	return res;
+    })
  };
