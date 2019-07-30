5b18357c7cbaacd29adde6449fba0ab7e8c8f503
ScarlettKK <scarlettkkszekai@gmail.com>
2019-07-30 17:23:36 +0800

    a start of queue

---
+++ code/Data Structure/Queue/task-scheduler.js
@@ -0,0 +1,9 @@
+/**
+ * @param {character[]} tasks
+ * @param {number} n
+ * @return {number}
+ * 题目: https://leetcode-cn.com/problems/task-scheduler/
+ */
+var leastInterval = function(tasks, n) {
+    
+};
--- code/Data Structure/Stack/maximal-rectangle.js
+++ code/Data Structure/Stack/maximal-rectangle.js
@@ -8,14 +8,26 @@
     // 至少两个1以上连着的部分
     let reg = /1{2,}/g
     // 把二维数组重新表达,将连续的相邻的1的位置提取出来(起始点+截止点)
-    result = matrix.map((item) => {
+    // [0,0,1,1,1,0,1,1,1] => [[2,4],[6,8]]
+    matrix = matrix.map((item) => {
     	let str = item.join('');
     	let r = reg.exec(str);
     	let res = []
+    	// 提取一行中所有连续的1
     	while(r) {
     		rs.push([r.index, r.index + r[0].length -1])
     		r = reg.exec(str);
     	}
     	return res;
     })
+    // 通过递归以及出栈压栈计算相邻的矩阵
+    // 通过两行起始点+截止点的交叉部分来计算交叉点
+    // 这里的n是拿来记录交叉计算了几行的,因为这里的交叉只能得到几列到几列
+    let maxRect = (arr, result, n=1) => {
+    	// 第一行第二行出栈
+    	let top = arr.pop();
+    	let next = arr.pop();
+    	// 再来计算相交的位置
+    	var start, end
+    }
  };
