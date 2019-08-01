c4c9b78c6c33242405d50b165b37a657dd803fb9
ScarlettKK <scarlettkkszekai@gmail.com>
2019-08-01 17:15:08 +0800

    a start of LinkedList

---
+++ code/Data Structure/LinkedList/linked-list-cycle.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ * 题目: https://leetcode-cn.com/problems/linked-list-cycle/
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ * 题目: https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    
+};
---
+++ code/Data Structure/LinkedList/sort-list.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ * 题目: https://leetcode-cn.com/problems/sort-list/
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    
+};
