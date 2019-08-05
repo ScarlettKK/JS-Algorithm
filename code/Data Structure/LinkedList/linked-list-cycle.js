099a0a31e2e3d21f08c617816c93c1b5b6e2d70d
ScarlettKK <scarlettkkszekai@gmail.com>
2019-08-05 17:55:09 +0800

    finish linked-list-cycle

--- code/Data Structure/LinkedList/linked-list-cycle.js
+++ code/Data Structure/LinkedList/linked-list-cycle.js
@@ -1,34 +1,98 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- * 题目: https://leetcode-cn.com/problems/linked-list-cycle/
+ * 题目: https://leetcode-cn.com/problems/design-circular-queue/
  */
 
 /**
- * @param {ListNode} head
+ * Initialize your data structure here. Set the size of the queue to be k.
+ * @param {number} k
+ */
+var MyCircularQueue = function(k) {
+    this.list = Array(k);
+    this.start = 0;
+    this.end = 0;
+    this.len = k;
+};
+
+/**
+ * Insert an element into the circular queue. Return true if the operation is successful. 
+ * @param {number} value
  * @return {boolean}
  */
-var hasCycle = function(head) {
-    
+MyCircularQueue.prototype.enQueue = function(value) {
+    if(this.isFull()){
+    	return false;
+    } else {
+    	this.list[this.end] = value;
+    	this.end = (this.end + 1) % this.len;
+    	return true;
+    }
 };
 
-
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- * 题目: https://leetcode-cn.com/problems/linked-list-cycle-ii/
+ * Delete an element from the circular queue. Return true if the operation is successful.
+ * @return {boolean}
  */
-
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-var detectCycle = function(head) {
-    
+MyCircularQueue.prototype.deQueue = function() {
+    if(this.isEmpty()){
+    	return false;
+    } else {
+    	let value = this.list[this.start];
+	    this.list[this.start] = null;
+	    // 循环队列,如果队列满了,首指针也要回到队首
+	    this.start = (this.start + 1) % this.len;
+	    return true;
+    }
 };
+
+/**
+ * Get the front item from the queue.
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function() {
+    if(!this.isEmpty())
+    	return this.list[this.start];
+    else
+    	return -1
+};
+
+/**
+ * Get the last item from the queue.
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function() {
+    if(!this.isEmpty()){
+    	if(this.end - 1 < 0)
+    		return this.list[this.len - 1]
+    	else
+    		return this.list[this.end - 1]
+    }
+    else
+    	return -1
+};
+
+/**
+ * Checks whether the circular queue is empty or not.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function() {
+    return this.start === this.end && !this.list[this.start]
+};
+
+/**
+ * Checks whether the circular queue is full or not.
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function() {
+    return this.start === this.end && !!this.list[this.start]
+};
+
+/** 
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
--- code/Data Structure/LinkedList/sort-list.js
+++ code/Data Structure/LinkedList/sort-list.js
@@ -11,5 +11,25 @@
  * @return {ListNode}
  */
 var sortList = function(head) {
-    
+    function sort (arr) {
+		let len = arr.length;
+		if(len < 2)
+			return arr;
+		else {
+			// 将第一个元素当成flag位置,比flag大的放在右边,小的放在左边,以此类推
+			let flag = arr[0].val;
+			let left = [];
+			let right = [];
+			for(let i = 1; i < len; i++) {
+				if(arr[i].val < flag)
+					left.push(arr[i])
+				else
+					right.push(arr[i])
+			}
+			// 最后返回排序好的左边+flag+排序好的右边
+			return sort(left).concat(flag, sort(right));
+		}
+	}
+
+	return sort(nums);
 };
