f7249db4daa112711832d9a5abb606dde967f185
ScarlettKK <scarlettkkszekai@gmail.com>
2019-08-12 15:54:08 +0800

    add comment

--- code/Regular Expression/the-Reg-in-work.js
+++ code/Regular Expression/the-Reg-in-work.js
@@ -1,6 +1,9 @@
+// 将字符串以|或者;分开
 string.split(/[|;]/)
 
+// 将字符串以||分开
 string.split(/\|{2}/)
 
-reg = new RegExp('(' + content.amount.join('|') + ')', 'g')
-content.content = content.content.replace(reg, '<span class="amount">$&</span>')
+// 将string内含有arr中任一字符串的部分,标记成为高亮
+reg = new RegExp('(' + arr.join('|') + ')', 'g')
+string = string.replace(reg, '<span style="color:red">$&</span>')
